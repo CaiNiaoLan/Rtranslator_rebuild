4a0291ef7f50fc23b342dce48f01c1de8690edff
CaiNiaoLan <l17346648981@163.com>
2026-06-15 18:55:46 +0800

    chore: project skeleton with CMake, Qt5, GTest, nlohmann/json

---
+++ qml/main.qml
@@ -0,0 +1,17 @@
+import QtQuick 2.15
+import QtQuick.Window 2.15
+
+Window {
+    width: 1280
+    height: 800
+    visible: true
+    title: "RTranslator"
+    color: "#1e1e2e"
+
+    Text {
+        anchors.centerIn: parent
+        text: "RTranslator"
+        color: "#cdd6f4"
+        font.pixelSize: 32
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    app.setOrganizationName("RTranslator");
+    app.setApplicationName("RTranslator");
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+
+    engine.load(url);
+    return app.exec();
+}
